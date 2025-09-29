b0d1440d8614958ceac28b3b90cd17929503b354
yaqoob75 <yaqoobdev75@gmail.com>
2025-09-29 15:21:07 +0500

    Ui Sidebar and Andmin Sttings done

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -13,11 +13,11 @@ const Sidebar = forwardRef(({ isOpen, setIsOpen }, ref) => {
         isOpen ? "translate-x-0" : "-translate-x-full"
       } transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:inset-0`}
     >
-      <div className="flex items-center justify-between h-16 px-6">
+      <div className="flex items-center justify-between h-16 px-8">
         <img
           src={sidebarLogo}
           alt="icon"
-          className="mt-4 ml-7 cursor-pointer"
+          className="mt-4 cursor-pointer"
           onClick={() => navigate("/dashboard")}
         />
         <button
